39b05c5268be645c816480acb1c2bdd9b07a5858
Fernanda Caramico <46379117+fernandacaramico@users.noreply.github.com>
2022-05-05 10:55:18 -0300

    Correção - Conversão DataHora em Script SQLServer

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,7 +1,20 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-    instrucaoSql = `select 
+    
+    instrucaoSql = ''
+    
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select 
+                        temperatura, 
+                        umidade, 
+                        momento,
+                        CONVERT(varchar, momento, 108) as momento_grafico
+                    from medida
+                    where fk_aquario = ${idAquario}
+                    order by id desc limit ${limite_linhas}`;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
                         temperatura, 
                         umidade, 
                         momento,
@@ -9,17 +22,38 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                     from medida
                     where fk_aquario = ${idAquario}
                     order by id desc limit ${limite_linhas}`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+    
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select 
+    
+    instrucaoSql = ''
+    
+    if (process.env.AMBIENTE_PROCESSO == "producao") {       
+        instrucaoSql = `select 
+                        temperatura, 
+                        umidade, CONVERT(varchar, momento, 108) as momento_grafico, 
+                        fk_aquario 
+                        from medida where fk_aquario = ${idAquario} 
+                    order by id desc limit 1`;
+        
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
                         temperatura, 
                         umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
                         fk_aquario 
                         from medida where fk_aquario = ${idAquario} 
                     order by id desc limit 1`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
