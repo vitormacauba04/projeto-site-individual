3147456a9b7c66e3eb45ce67f140beab2c6d1655
fernanda caramico 123 <fernandacaramico@gmail.com>
2022-05-15 22:23:07 -0300

    Melhoria - login de usuário em div #b_usuario - retira div#h1

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -5,14 +5,10 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var h1LoginUsuario = document.getElementById("h1_login_usuario");
     var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
-        if (h1LoginUsuario != undefined) {
-            h1LoginUsuario.innerHTML = email;
-        }
         b_usuario.innerHTML = nome;
 
         // finalizarAguardar();
