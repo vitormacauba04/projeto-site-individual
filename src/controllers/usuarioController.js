627142bb512a6415313f3049c3d2b8a023079910
vitor.macauba04 <vitor.macauba@sptech.school>
2022-05-19 11:33:41 -0300

    Primeiro commit do site já bem avançado

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -65,6 +65,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var musica = req.body.musicaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -76,7 +77,7 @@ function cadastrar(req, res) {
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, musica)
             .then(
                 function (resultado) {
                     res.json(resultado);
